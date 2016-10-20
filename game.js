d48c6bc4c9929d4aa9af9aec4d219e3c060dd761
Aaron Lee <worldedit05@gmail.com>
2016-10-19 22:35:21 -0500

    Selecting a random word is working

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
 // This file will randomly select a word for the player
+
+var wordObj = {
+  words: ['desting','ghost','vex','taken', 'warlock'],
+  message: {
+    win: 'You win!',
+    lose: 'Game Over.',
+    warning: 'You have already guessed this letter: '
+  },
+
+  randomWord: function() {
+
+    var answerWord = wordObj.words[Math.floor(Math.random() * wordObj.words.length)];
+
+    return answerWord;
+  }
+};
+
+module.exports = wordObj;
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // This is the main file. It will contain the logic of the app. Running it in Terminal/Bash will start the game
+
+var word = require('./game.js');
+
+console.log(word.randomWord());
